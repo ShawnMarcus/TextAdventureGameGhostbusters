6632adf991e1f3dec2e41af5bac5c3c1d5b5bc24
Shawn Marcus <smarcus@post.com>
2021-09-28 11:13:12 -0400

    Added nested conditional statements for game functionality. Initial version of project complete.

--- script.js
+++ script.js
@@ -33,3 +33,51 @@ OR
 ${enter}`;
 const gameOver5 = `You looked at the trap and the light BLINDED YOU! ${gameOver}`;
 const win = `The ghost trap sucked Slimer in and closed. CONGRATULATIONS! YOU CAUGHT SLIMER!!!`;
+
+//Alerts the user to start the game
+alert(start);
+
+//Sets the value of "userInput" to the user's entry
+let userInput = prompt(q1);
+
+//1st Conditional Statement
+//NOTE: Prompt returns the value entered as a string (in case the user enters 1 or "1" in a string). That is why double equals == instead of triple === (strict equality) here. 
+if (userInput == 1) {
+    userInput = prompt (q2);
+
+    //2nd Conditional Statement
+    if (userInput == 1) {
+       userInput = prompt (q3);
+
+        //3rd Conditional Statement
+        if (userInput == 2) {
+            userInput = prompt (q4);
+
+            //4th Conditional Statement
+            if (userInput == 1) {
+            userInput = prompt (q5);
+
+                //5th Conditional Statement
+                if (userInput == 2) {
+                    alert (win);
+
+                } else {
+                    alert (gameOver5);
+                }
+
+            } else {
+                alert (gameOver4);
+            }
+
+        } else {
+            alert (gameOver3);
+        }
+
+    } else {
+        alert (gameOver2);
+    }
+
+} else {
+    alert(gameOver1);
+}
+    
